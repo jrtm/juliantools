7ff4f2095345cdf7031f89056cf9706b8b09e330
Julian Ravn Thrap-Meyer <jr@thrap-meyer.no>
2024-06-05 14:50:24 +0200

    Test pushing dummy secrets to a public repo with secret scanning enabled
    These secrets were copied from this repo:
    https://github.com/advanced-security/secret-scanning-custom-patterns

--- README.md
+++ README.md
@@ -35,3 +35,9 @@ $ å
 $ å 3
 Switched to branch 'third-branch'
 ```
+
+
+### Test
+
+Here's a dummy key:
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
---
+++ script.js
@@ -0,0 +1,13 @@
+// first a sendgrid key
+const key = "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";
+
+// then a basic datadog key
+const datadog = "01000000000000000000000000000000";
+
+// perhaps a webhook?
+const whUrl =
+    "https://example.webhook.office.com/webhookb2/892e8ed3-997c-4b6e-8f8a-7f32728a8a87@f7322380-f203-42ff-93e8-66e266f6d2e4/IncomingWebhook/fcc6565ec7a944928bd43d6fc193b258/4f0482d4-b147-4f67-8a61-11f0a5019547";
+
+// and maybe an azure client secret
+const clientSecret = "kc3KP75a~edN1cV.rs~_r6F-O7-C-55-Ts"
+
